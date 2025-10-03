6b1892c192897837236a13e5dd43a8c29591f039
Muhammad Ameen <mohammadameen786110@gmail.com>
2025-10-03 17:56:13 +0500

    Allow CROSS

--- TekRemittance.Web/Program.cs
+++ TekRemittance.Web/Program.cs
@@ -20,7 +20,19 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
